f81effb39dc5a5a9a1fae801bcecb7eadb713bbd
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-01-30 19:46:54 +0000

    Update user authentication logic

--- src/include/functions/dbConnect.inc.php
+++ src/include/functions/dbConnect.inc.php
@@ -1,19 +1,19 @@
 <?php
 
 function db_connect() {
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "DDOC";
-    
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:ddoc-lets-run.database.windows.net,1433; Database = DDOC", "RafaelAdmin", "Rafa1402");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
     }
     
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "RafaelAdmin", "pwd" => "Rafa1402", "Database" => "DDOC", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:ddoc-lets-run.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
     return $conn;
 }
 
