e99d3bb9f2a5e4f09377b3bd64c00b08aa2a6ea5
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-20 19:42:04 +0100

    New unknown user

--- src/include/functions/SQLfunctions.inc.php
+++ src/include/functions/SQLfunctions.inc.php
@@ -266,7 +266,7 @@
                 $biography = $row['user_biography'];
         
                 if (!$profilePic) {
-                    $profilePic = 'https://th.bing.com/th/id/R.3e77a1db6bb25f0feb27c95e05a7bc57?rik=DswMYVRRQEHbjQ&riu=http%3a%2f%2fwww.coalitionrc.com%2fwp-content%2fuploads%2f2017%2f01%2fplaceholder.jpg&ehk=AbGRPPcgHhziWn1sygs8UIL6XIb1HLfHjgPyljdQrDY%3d&risl=&pid=ImgRaw&r=00';
+                    $profilePic = $arrConfig['url_users']. "Unknown_person.jpg";
                 }
                 else{
                     $profilePic = $arrConfig['url_users']. $profilePic ;
