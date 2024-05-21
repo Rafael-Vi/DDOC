6cc6d66df90086448491f9e1075a26ed78569442
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-21 19:10:13 +0100

    a

--- src/index.php
+++ src/index.php
@@ -56,27 +56,13 @@
   </li>
   <li>
     <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
     <div class="timeline-start timeline-box text-4xl">Ranking temático</div>
     <div class="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
     <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
   </li>
   <li>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
     <hr/>
     <div class="timeline-start timeline-box text-4xl">Ranks de Contas</div>
     <div class="timeline-middle">
@@ -85,11 +71,6 @@
     <hr/>
   </li>
   <li>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
-    <hr/>
     <hr/>
     <div class="timeline-start timeline-box text-4xl">Mensagens</div>
     <div class="timeline-middle">
