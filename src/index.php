c94bc66ed827cf1f86c8b01e7c9974c28a8fcafd
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-21 19:04:09 +0100

    yes

--- src/index.php
+++ src/index.php
@@ -87,9 +87,8 @@
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Hello there</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <h1 class="text-5xl font-bold">Agora que sabes no que te vais meter</h1>
+      <button class="btn btn-primary">Entra</button>
     </div>
   </div>
 </div>
