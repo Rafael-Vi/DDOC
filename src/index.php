f9bbda103f3ebecc6498d5987afa225de1725849
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-21 11:38:43 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -21,28 +21,29 @@
 <body class="bg-gray-800">
 
 <section class="w-full h-screen flex flex-col sm:flex-row items-center justify-center" id="welcome-ddoc">
-<!-- Updated First div -->
-<div class="flex flex-col w-1/2 p-8 flex items-center justify-center lg:ml-auto">
-    <div class="text-7xl text-white ubuntu-bold hidden lg:block"> 
-        WELCOME TO
+    <!-- First div -->
+    <div class="flex flex-col w-1/2 p-8 flex items-center justify-center lg:ml-auto">
+        <div class="text-9xl text-white ubuntu-bold hidden lg:block"> 
+            WELCOME TO
+        </div>
+        <div class="flex items-center justify-center lg:justify-right flex-row">
+            <p class="text-6xl text-white ubuntu-bold">D</p>
+            <p class="text-6xl mx-1 text-white ubuntu-bold">D</p>
+            <div class="loader border-t-8 rounded-full border-t-red-400 bg-orange-300 animate-spin aspect-square w-24 flex justify-center items-center text-yellow-700"></div>
+            <p class="mx-1 my-8 text-6xl text-white ubuntu-bold">C</p>
+        </div>
     </div>
-    <div class="flex items-center justify-center lg:justify-right flex-row">
-        <p class="text-7xl text-white ubuntu-bold">D</p>
-        <p class="text-7xl mx-1 text-white ubuntu-bold">D</p>
-        <div class="loader border-t-8 rounded-full border-t-red-400 bg-orange-300 animate-spin aspect-square w-24 flex justify-center items-center text-yellow-700"></div>
-        <p class="mx-1 my-8 text-7xl text-white ubuntu-bold">C</p>
-    </div>
-</div>
 
-<!-- Updated Second div -->
-<div class="w-1/2 p-8 flex items-start justify-start">
-    <ul class="text-white w-42">
-        <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="./accountLC.php">Entra...</a></li>
-        <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Funcionalidades</a></li>
-        <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Manual</a></li>
-        <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Contactar Creador</a></li>
-    </ul>
-</div>
+    <!-- Second div -->
+    <div class="w-1/2 p-8 flex items-start justify-start">
+        <ul class="text-white w-42">
+            <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="./accountLC.php">Entra...</a></li>
+            <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Funcionalidades</a></li>
+            <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Manual</a></li>
+            <li><a class="btn m-2 p-2 text-2xl w-full hover:scale-110 hover:border hover:border-orange-500 hover:b-2" href="#">Contactar Creador</a></li>
+        </ul>
+    </div>
+</section>
 
     
 <script src="../dist/bundle.js"></script>
