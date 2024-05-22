f5eb58550897cf5e7160d9916e774cb314cba2a0
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-22 16:42:31 +0100

    Email?

--- src/include/functions/SQLfunctions.inc.php
+++ src/include/functions/SQLfunctions.inc.php
@@ -137,26 +137,26 @@
     //!QUERY OPTIMIZATION ----------------------------------------------------------------
 
 
-function sendVerificationEmail($to, $subject, $message, $link) {
-    // Define the headers
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers.= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers.= "From: <rafa.pinto.vieira@gmail.com>\r\n";
-    
-    // Define the message
-    $body = "<html><body>";
-    $body.= "<p>Hello,</p>";
-    $body.= "<p>Please click the link below to verify your email:</p>";
-    $body.= "<a href=\"$link\">Verify Email</a></p>";
-    $body.= "</body></html>";
+    function sendVerificationEmail($to, $subject, $message, $link) {
+        // Define the headers
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers.= "Content-Type: text/html; charset=UTF-8\r\n";
+        $headers.= "From: DDOC <rafa.pinto.vieira@gmail.com>\r\n";
+        
+        // Define the message
+        $body = "<html><body>";
+        $body.= "<p>Hello,</p>";
+        $body.= "<p>Please click the link below to verify your email:</p>";
+        $body.= "<a href=\"$link\">Verify Email</a></p>";
+        $body.= "</body></html>";
 
-    // Send the email
-    if(mail($to, $subject, $body, $headers)) {
-        return true;
-    } else {
-        return false;
+        // Send the email
+        if(mail($to, $subject, $body, $headers)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
 
 
 
