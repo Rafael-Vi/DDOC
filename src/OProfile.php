a2959e6964c8c1917fed1fd8b0c4832273703181
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-22 21:43:49 +0100

    Other PErson Profile

--- src/OProfile.php
+++ src/OProfile.php
@@ -23,7 +23,7 @@ $userInfo = getUserInfo($_GET['userid']);
     <link rel="stylesheet" href="../src/css/social.css">    <link rel="shortcut icon" href="./assets/images/2.png" >
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     <script src="https://cdn.tailwindcss.com"></script>     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css" rel="stylesheet" type="text/css" />
-    <title><?php var_dump($userInfo['username'])?></title>
+    <title><?php echo $userInfo['username']; ?></title>
 </head>
 <body class="h-full flex">
     <?php if ($_SESSION['uid'] == $_GET['userid']) {
