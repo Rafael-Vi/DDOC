017af5d75c85be16d6e61e9f2d8a41f5e459212a
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-05-22 13:19:48 +0100

    Update SQLfunctions.inc.php

--- src/include/functions/SQLfunctions.inc.php
+++ src/include/functions/SQLfunctions.inc.php
@@ -141,8 +141,8 @@ function sendVerificationEmail($to, $subject, $message, $link) {
     // Define the headers
     $headers = "MIME-Version: 1.0\r\n";
     $headers.= "Content-Type: text/html; charset=UTF-8\r\n";
-    $headers.= "From: Your Website <noreply@yourwebsite.com>\r\n";
-
+    $headers.= "From: DDOC <rafa.pinto.vieira@gmail.com>\r\n";
+    
     // Define the message
     $body = "<html><body>";
     $body.= "<p>Hello,</p>";
