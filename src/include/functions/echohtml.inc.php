ba99b14f647d774366b1caa1817cdeb69f9296fd
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-08 17:07:10 +0100

    Revert a lot

--- src/include/functions/echohtml.inc.php
+++ src/include/functions/echohtml.inc.php
@@ -11,32 +11,24 @@
         }
         echo '</div>';
     }
-
     function echoProfileInfo($username, $email, $profilePic, $realName, $biography, $rank){
         global $arrConfig;
-        // Container for profile info and image with additional padding and defined max-width for larger screens
-        echo '<div class="flex flex-col md:flex-row items-start md:items-center w-full max-w-4xl mx-auto p-4 bg-gray-800 rounded-lg">';
-        
-        // Profile image with conditional classes for size adjustment
-        echo '<div class="avatar mr-8 mb-4 md:mb-0">';
-        echo '<div class="w-36 h-36 md:w-48 md:h-48 mask mask-squircle">';
-        echo '<img src="'.$profilePic.'" alt="Profile Picture">';
-        echo '</div>';
-        echo '</div>';
-        
-        // User information with additional margin for top alignment
-        echo '<div class="flex-1">';
-        echo '<div class="block text-3xl sm:text-4xl font-bold text-amber-500">Rank: #' . $rank . '</div>';
+        echo '<div class="block text-3xl sm:text-4xl font-bold text-amber-500">Rank: #' ."$rank" . '</div>';
         echo '<span class="block font-bold text-3xl mt-4 text-amber-700 mb-4">@' . $username . '</span>';
-        echo '<div class="font-bold text-white">' . $realName . '</div>';
+        echo '<div class="font-bold  text-white">' . $realName . '</div>';
         echo '<div class="w-full text-white">' . $biography . '</div>';
-        echo '<div class="sm:flex sm:space-x-4 mt-4">';
+        echo '<div class="sm:flex sm:space-x-4 relative m-auto sm:float-right sm:mt-4">';
         echo '<a href="#" onclick="showFollow(\'follower\'); return false;"><div id="followers-count" class="font-bold">Seguidores: ---</div></a>';
         echo '<a href="#" onclick="showFollow(\'following\'); return false;"><div id="following-count" class="font-bold">A seguir: ---</div></a>';
         echo '</div>';
-        echo '</div>'; // Close user information container
-        
-        echo '</div>'; // Close main container
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="relative flex flex-col mt-8 mb-auto">';
+        echo '<div class="avatar">';
+        echo '<div class="w-36 mask mask-squircle">';
+        echo '<img src="'.$profilePic.'" />';
+        echo '</div>';
+        echo '</div>';
     }
 
     function echoUserPosts($post) {
