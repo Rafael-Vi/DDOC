3d416a030abde4010a7533674454f17644cc61f5
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-08 18:28:00 +0100

    It was alright before

--- src/js/Notification.js
+++ src/js/Notification.js
@@ -42,20 +42,12 @@ function loadNotifications() {
         url: '../src/include/functions/SQLfunctions.inc.php',
         type: 'POST',
         data: { function: 'loadNotifications' },
-        success: function(response) {
-            // Assuming the response is a JSON string that needs to be parsed
-            const data = JSON.parse(response);
-            // Check if the count of notifications is more than 0
-            if (data.count > 0 && data.html !== lastData) {
+        success: function(data) {
+            // Only update if the data has changed
+            if (data !== lastData) {
                 const $notificationsContainer = $('#notifications-container');
-                // Update the notifications container with the HTML from the response
-                $notificationsContainer.html(data.html);
-                lastData = data.html;
-                // Show the delete all notifications button
-                $('#delete-all-notif').show();
-            } else {
-                // Hide the delete all notifications button if there are no notifications
-                $('#delete-all-notif').hide();
+                $notificationsContainer.html(data);
+                lastData = data;
             }
         },
         error: function(error) {
@@ -63,7 +55,5 @@ function loadNotifications() {
         }
     });
 }
-
-setInterval(loadNotifications, 500);
 
 setInterval(loadNotifications, 500);
