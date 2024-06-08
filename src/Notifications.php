c879638d72f0535814161d8909a216a3e4ea1950
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-08 18:19:46 +0100

    What about now

--- src/Notifications.php
+++ src/Notifications.php
@@ -14,23 +14,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/social.css">    <link rel="shortcut icon" href="./assets/images/2.png" >
+    <link rel="stylesheet" href="../src/css/social.css">
+    <link rel="shortcut icon" href="./assets/images/2.png">
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-      <link href="/dist/tailwind.css" rel="stylesheet" type="text/css" />
+    <link href="/dist/tailwind.css" rel="stylesheet" type="text/css" />
     <title>Notifications</title>
 </head>
 <body class="h-full flex">
-    <?php echoLoadScreen(); ?>    <?php
+    <?php echoLoadScreen(); ?>
+    <?php
         //echoShowTheme();
     ?>
     <?php echoNav(); ?>
-    < id="notifications-div" class="bg-gray-900 fixed flex flex-col h-full w-full md:w-9/12 p-0 m-0 md:right-0">
+    <div id="notifications-div" class="bg-gray-900 fixed flex flex-col h-full w-full md:w-9/12 p-0 m-0 md:right-0">
         
       <div class="h-32 text-center sm:text-start w-full p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 backdrop-blur-md">
-  <a href="javascript:history.back()" class="btn">Voltar atrás</a>
+        <a href="javascript:history.back()" class="btn">Voltar atrás</a>
         Notifications
-
-
       </div>
 
         <div class="mt-8 mb-8 float-right" id="delete-all-notif">
@@ -40,18 +40,17 @@
         <?php setNotifRead()?>
         <?php getNotif(); ?>
         </div>
-
     </div>
     <?php echoBottomNav(); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script>
-    var themes = <?php echo json_encode($_SESSION['themes'][0]['finish_date']); ?>;
-  </script>
-  <script>
-  var targetDateFromPHP = <?php echo json_encode($_SESSION['themes'][0]['finish_date']); ?>;
-</script>
-  <script src="../src/js/timer.js"></script>
-  <script src="../src/js/Notification.js"></script>
-  <script src="../src/js/social.js"></script>
+    <script>
+      var themes = <?php echo json_encode($_SESSION['themes'][0]['finish_date']); ?>;
+    </script>
+    <script>
+      var targetDateFromPHP = <?php echo json_encode($_SESSION['themes'][0]['finish_date']); ?>;
+    </script>
+    <script src="../src/js/timer.js"></script>
+    <script src="../src/js/Notification.js"></script>
+    <script src="../src/js/social.js"></script>
 </body>
 </html>
