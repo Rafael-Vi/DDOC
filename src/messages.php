a27f6ec55f26fe66175150484ca96adcd1b10455
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-15 15:55:08 +0100

    Messages Corrected?

--- src/messages.php
+++ src/messages.php
@@ -1,39 +1,37 @@
 <?php
 include "include/config.inc.php";
-
-  include "include/functions/checkLogin.inc.php";
-
-      require "include/functions/checkThemeIsFinished.inc.php";
-
-     if (checkThemeIsFinished()){
+include "include/functions/checkLogin.inc.php";
+require "include/functions/checkThemeIsFinished.inc.php";
+if (checkThemeIsFinished()){
     include "include/functions/saveLastPage.inc.php";
-  }
-  require "include/functions/Development.inc.php";
+}
 
-     if (isset($_GET['convo_id']) && $_GET['convo_id'] != "") {
-        // Get all conversation IDs
-        $convoIds = getConvo();
-        if (in_array($_GET['convo_id'], $convoIds)) {
-            // Define a random message ID, message, date, and sender
-            $userDetails = getUserDetails($_GET['convo_id']);
-            $username = $userDetails['username'];
-            $profilePic = $userDetails['profile_pic'];
+require "include/functions/Development.inc.php";
 
-            
-            $sender = [
-                'username' => $username,
-                'profile_pic' => $profilePic
-            ];
+// Reset session variables if no specific convo_id is provided
+if (!isset($_GET['convo_id']) || $_GET['convo_id'] == "") {
+    unset($_SESSION['sender']);
+    unset($_SESSION['convo_id']);
+} else {
+    // Existing logic to handle specific convo_id
+    $convoIds = getConvo();
+    if (in_array($_GET['convo_id'], $convoIds)) {
+        $userDetails = getUserDetails($_GET['convo_id']);
+        $username = $userDetails['username'];
+        $profilePic = $userDetails['profile_pic'];
         
-            $_SESSION['sender'] = $sender;
-            $_SESSION['convo_id'] = $_GET['convo_id'];
-        } else {
-            header("Location: messages.php");
-            exit; // Ensure no further output is sent
-        }
+        $sender = [
+            'username' => $username,
+            'profile_pic' => $profilePic
+        ];
+    
+        $_SESSION['sender'] = $sender;
+        $_SESSION['convo_id'] = $_GET['convo_id'];
     } else {
-        
+        header("Location: messages.php");
+        exit; // Ensure no further output is sent
     }
+}
 ?>
 
 <!DOCTYPE html>
