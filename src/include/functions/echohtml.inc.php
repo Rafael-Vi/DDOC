9f021e23f96d3dbdb73b9f9c1cf62f54a1f4abea
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-15 19:00:56 +0100

    Rotilhão

--- src/include/functions/echohtml.inc.php
+++ src/include/functions/echohtml.inc.php
@@ -12,18 +12,16 @@
         echo '</div>';
     }
 
-
     function echoProfileInfo($username, $email, $profilePic, $realName, $biography, $rank){
         global $arrConfig;
         echo '<div class="profile-info-container flex flex-row-reverse text-right">';
         // Text Information
-        echo '<div class="text-info w-full">';
-        echo '<div class="block text-3xl sm:text-4xl font-bold text-amber-500 text-right">Rank: #' ."$rank" . '</div>';
-        echo '<span class="block font-bold text-3xl mt-4 text-amber-700 mb-4 text-right">@' . $username . '</span>';
-        echo '<div class="font-bold text-white text-right">' . $realName . '</div>';
-        echo '<div class="w-full text-white text-right">' . $biography . '</div>';
-        // Following and Followers Information
-        echo '<div class="sm:flex sm:space-x-4 relative m-auto">';
+        echo '<div class="text-info w-full text-right">'; // Ensure text alignment is right
+        echo '<div class="block text-3xl sm:text-4xl font-bold text-amber-500">Rank: #' ."$rank" . '</div>';
+        echo '<span class="block font-bold text-3xl mt-4 text-amber-700 mb-4">@' . $username . '</span>';
+        echo '<div class="font-bold text-white">' . $realName . '</div>';
+        echo '<div class="w-full text-white">' . $biography . '</div>';
+        echo '<div class="sm:flex sm:space-x-4 relative m-auto text-right">'; // Adjust alignment for following/followers
         echo '<a href="#" onclick="showFollow(\'follower\'); return false;"><div id="followers-count" class="font-bold">Seguidores: ---</div></a>';
         echo '<a href="#" onclick="showFollow(\'following\'); return false;"><div id="following-count" class="font-bold">A seguir: ---</div></a>';
         echo '</div>';
