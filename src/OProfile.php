11d2e7f572326458ea74e446a9156c14beb34528
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-17 18:57:51 +0100

    Nova Correção

--- src/OProfile.php
+++ src/OProfile.php
@@ -39,27 +39,26 @@ $userInfo = getUserInfo($_GET['userid']);
           <div class="flex  h-32 text-white lg:h-64 mt-8 w-4/6">
               <div class="h-full w-full mt-0 md:mt-8 mb-4">
                 <?php
-                    echoProfileInfo($userInfo['username'], $userInfo['email'], $userInfo['profilePic'], $userInfo['realName'], $userInfo['biography'], $userInfo['rank']);
-                    unset($userInfo);
-                    
-                ?>  
-
-             <div class="flex justify-center">
-
-
-                      <a <?php echo 'href="./messages.php?convo_id=' . $_GET['userid'] . '"';?> class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4" >
-                          Message
+                  echoProfileInfo($userInfo['username'], $userInfo['email'], $userInfo['profilePic'], $userInfo['realName'], $userInfo['biography'], $userInfo['rank']);
+                  unset($userInfo);
+                  echo '
+                    <div class="flex justify-center">
+                      <a href="./messages.php?convo_id=' . $_GET['userid'] . '" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4">
+                        Message
                       </a>
-                    
                       <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-4" id="follow-button" onclick="followCheck()">
-                          Follow
+                        Follow
                       </button>
+                    </div>
+                  ';
+                  echo '</div>';
+                  echo '</div>';
+                ?>
 
 
-            </div>
             </div>
            </div>
-       <div id="profilePosts-div" class="relative p-auto overflow-auto bg-base-200 min-h-screen">
+       <div id="profilePosts-div" class="relative w-full p-auto overflow-auto bg-base-200 min-h-screen">
          <?php
             getPosts($_GET['userid']);
          ?>
