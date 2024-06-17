a2c3e3d359af19bf8fe2f3eb8b0c6536a2e3d047
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-18 00:41:16 +0100

    Roto Ferras

--- admin/includes/nav.inc.php
+++ admin/includes/nav.inc.php
@@ -1,6 +1,6 @@
 <div class="navbar bg-gray-800">
     <div class="navbar-start">
-        <a href="index.php" class="btn btn-ghost text-warning btn-circle">
+        <a href="index.php" class="btn btn-ghost text-warning btn-circle mx-8">
             <i class="fi fi-br-house-chimney"></i>
         </a>
     </div>
@@ -20,7 +20,7 @@
                     <!-- USER/ADMIN -->
                 </div>
             </div>
-            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">   
+            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 mx-8">   
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>
