967c4d061ad1e6e266ec1759a74c5a9c084e11d5
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-18 00:43:17 +0100

    Yay

--- admin/includes/nav.inc.php
+++ admin/includes/nav.inc.php
@@ -20,7 +20,7 @@
                     <!-- USER/ADMIN -->
                 </div>
             </div>
-            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 mx-8">   
+            <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-warning rounded-box w-52">   
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>
