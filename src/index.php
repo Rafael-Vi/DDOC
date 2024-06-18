067d79ba8d79308001d1038bf39279ef89b28105
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-18 01:07:52 +0100

    Yes

--- src/index.php
+++ src/index.php
@@ -34,7 +34,7 @@
 </head>
 <body class="bg-gray-800">
 
-<div class="navbar bg-gray-200 min-h-16 transition-all flex items-center justify-between">
+<div class="navbar bg-base-100 min-h-16 transition-all flex items-center justify-between">
   <div class="flex items-center">
     <button class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block text-orange-500 w-10 h-10 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
