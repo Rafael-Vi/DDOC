b11d4009cb147567466408f27d2cb2dfd922ca3a
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-18 01:01:28 +0100

    hwy

--- src/index.php
+++ src/index.php
@@ -19,7 +19,11 @@
   <style>
     /* CSS code */
     .navbar {
-      display: hidden; /* Initially hide the navbar */
+      position: fixed; /* Make the navbar fixed at the top */
+      top: 0; /* Position it at the top */
+      left: 0; /* Align it to the left */
+      width: 100%; /* Make it occupy the full width */
+      z-index: 1000; /* Ensure it stays on top of other content */
     }
 
     .navbar.visible {
@@ -71,21 +75,7 @@
 </section>
 <div class="flex justify-center items-center min-h-screen">
   <section id="functions" class="flex flex-col">
-<div class="navbar bg-base-100">
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-    </button>
-  </div>
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-  </div>
-</div>
+
   </section>
 </div>
 
