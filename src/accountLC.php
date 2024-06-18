97d52f57d42f4b56030ce6165525c98fd8896b03
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-18 21:37:02 +0100

    what

--- src/accountLC.php
+++ src/accountLC.php
@@ -14,7 +14,7 @@ include "include/config.inc.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../src/css/accountLC.css">
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="/dist/tailwind.css" rel="stylesheet" type="text/css" />
     <title>Login & Register</title>
 </head>
 <body class="bg-gray-900">
