3e7cdd3f25cfad9fd866e059d372c7ef77a44c49
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-19 02:01:32 +0100

    Wiorkign hopefully

--- src/include/functions/echohtml.inc.php
+++ src/include/functions/echohtml.inc.php
@@ -58,7 +58,7 @@
         }
     
         echo '</a>';
-        echo '<button disabled class="new-button absolute top-0 right-0 m-2 btn-ghost btn text-white font-bold py-2 px-4 mr-8 rounded" style="visibility: hidden; margin-right: 80px;">'.htmlspecialchars($post['caption']).'</button>'; // New disabled button with unique class
+        echo '<button disabled class="new-button absolute top-0 right-0 m-2 bg-slate-800 text-white font-bold py-2 px-4 mr-8 rounded" style="visibility: hidden; margin-right: 80px;">'.htmlspecialchars($post['caption']).'</button>'; // New disabled button with unique class
         echo '<button src="https://cdn-icons-png.flaticon.com/512/5400/5400852.png" onclick="showMyModal(\'' . htmlspecialchars(addslashes($post['post_id']), ENT_QUOTES) . '\', \'' . htmlspecialchars(addslashes($post['caption']), ENT_QUOTES) . '\')" class="edit-post absolute top-0 right-0 m-2 bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" data-id="' . htmlspecialchars(urlencode($post['post_id']), ENT_QUOTES) . '" style="visibility: hidden;">Edit</button>';
         echo '</div>';  
     }
@@ -289,7 +289,7 @@ $imageHtml = '';
                         echo '<button id="like-button-' . $post['post_id'] . '" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onclick="likeCheck(' . $post['post_id'] . ')">Like</button>';
                     } else {
                         // Output a disabled button
-                        echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-primary w-16 text-white font-bold py-2 px-4 rounded" disabled>Like</button>';
+                        echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-ghost w-16 text-white font-bold py-2 px-4 rounded" disabled>Disabled</button>';
                     }
                     echo'
                     <span class="text-white text-2xl font-bold" id="like-count-'.$post['post_id'].'">Gostos: <span class="loading loading-ring loading-lg text-warning"></span></span>
@@ -329,7 +329,7 @@ $imageHtml = '';
                         echo '<button id="like-button-' . $post['post_id'] . '" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onclick="likeCheck(' . $post['post_id'] . ')">Like</button>';
                     } else {
                         // Output a disabled button
-                        echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-primary w-16 text-white font-bold py-2 px-4 rounded" disabled>Like</button>';
+                       echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-primary w-16 text-white font-bold py-2 px-4 rounded btn btn-ghost" disabled>Like</button>';
                     }
                     echo'
                      <span class="text-white text-2xl font-bold" id="like-count-'.$post['post_id'].'">Gostos: <span class="loading loading-ring loading-lg text-warning"></span></span>
@@ -365,7 +365,7 @@ $imageHtml = '';
                         echo '<button id="like-button-' . $post['post_id'] . '" class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onclick="likeCheck(' . $post['post_id'] . ')">Like</button>';
                     } else {
                         // Output a disabled button
-                        echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-primary w-16 text-white font-bold py-2 px-4 rounded" disabled>Like</button>';
+                       echo '<button id="like-button' . $post['post_id'] . '" class="btn btn-primary w-16 text-white font-bold py-2 px-4 rounded btn btn-ghost" disabled>Like</button>';
                     }
                     echo'
                      <span class="text-white text-2xl font-bold" id="like-count-'.$post['post_id'].'">Gostos: <span class="loading loading-ring loading-lg text-warning"></span></span>
