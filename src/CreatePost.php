c110a796513b68bf0fbb320cf1db38bc3fbde0e5
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-19 12:27:34 +0100

    Ver

--- src/CreatePost.php
+++ src/CreatePost.php
@@ -77,7 +77,7 @@
 </div>
 <label for="post-title" class="text-white font-bold text-2xl pt-4 text-left w-full">Legenda:</label>
 <textarea id="post-title" name="post-title" required class="rounded-lg px-4 py-2 bg-slate-600 text-white w-full" maxlength="255"></textarea><br>
-<div class="flex flex-row justify-between items-center gap-4 mt-8 w-full"> <!-- Ensure this is flex-row and items are centered -->
+<div class="flex flex-row justify-start items-center gap-4 mt-8 w-full"> <!-- Ensure this is flex-row and items are centered -->
   <div class="flex flex-col w-1/2"> <!-- This is already set to take half width -->
     <label for="post-type" class="text-white font-bold text-left">Tipo:</label>
     <select id="post-type" name="post-type" required class="rounded-lg px-4 py-2 bg-slate-600 text-white mb-4 w-full">
