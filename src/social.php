1561ba9ce2a71781fa49295934b7a1c09c9ba92f
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-19 16:08:08 +0100

    What about now

--- src/social.php
+++ src/social.php
@@ -34,7 +34,7 @@ require "include/functions/Development.inc.php";
   <title>DDOC</title>
 </head>
 
-< class="h-full flex">
+<body class="h-full flex">
   <?php echoLoadScreen(); ?>
   <?php echoNav(); ?>
   <?php
@@ -57,7 +57,7 @@ require "include/functions/Development.inc.php";
   }
   ?>
   <div class="bg-gray-900 fixed w-full md:w-9/12 p-0 m-0 md:right-0 h-full flex flex-col justify-center items-center" id="home-div"></div>
-  <div class="h-32 text-center sm:text-start w-full p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 shadow-mdbackdrop-blur-md">
+  <div class="h-32 text-center sm:text-start w-full p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 shadow-md backdrop-blur-md"></div>
     <a href="javascript:history.back()" class="btn">Voltar atrás</a>
     Home
   </div>
@@ -97,7 +97,7 @@ require "include/functions/Development.inc.php";
       </form>
     </div>
   </dialog>
-</div>
+  
   <?php echoBottomNav(); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
