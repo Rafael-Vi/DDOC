a392d58ee4a3fa1d163d9672cf6b30221c9e78bc
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-19 12:16:51 +0100

    Nice

--- src/CreatePost.php
+++ src/CreatePost.php
@@ -78,8 +78,8 @@
   </div>
   <label for="post-title" class="text-white font-bold text-2xl pt-4 text-left w-full">Legenda:</label>
   <textarea id="post-title" name="post-title" required class="rounded-lg px-4 py-2 bg-slate-600 text-white w-full" maxlength="255"></textarea><br>
-  <div class="flex flex-row justify-between items-center gap-4">
-  <div class="flex flex-col w-1/2">
+  <div class="flex flex-row justify-between items-start gap-4 mt-8">
+  <div class="flex flex-col w-1/2 h-full">
       <label for="post-type" class="text-white font-bold text-left">Tipo:</label>
       <select id="post-type" name="post-type" required class="rounded-lg px-4 py-2 bg-slate-600 text-white mb-4 w-full">
         <option value="audio">Audio</option>
@@ -89,7 +89,7 @@
     </div>
     <div class="flex flex-col w-1/2">
       <label for="file-input" class="text-white font-bold w-full text-left ">Upload de Ficheiro (Musica/Imagem/Vídeo):</label>
-      <input type="file" id="file-input" name="file-input" class="file-input file-input-bordered file-input-warning max-w-xs w-full">
+      <input type="file" id="file-input" name="file-input" class="file-input file-input-bordered text-warning file-input-warning max-w-xs w-full">
     </div>
   </div>
   <input type="text" id="post-theme" name="post-theme" disabled class="rounded-lg bg-slate-600 text-white p-2 w-full" value="Tema: <?php echo $_SESSION['themes'][0]['theme']; ?>">
