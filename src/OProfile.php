5d3c5668e5e7347d69d9d8c887d820ec61f4f4fb
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-19 01:56:48 +0100

    idk should work

--- src/OProfile.php
+++ src/OProfile.php
@@ -90,7 +90,7 @@ $userInfo = getUserNotCurrent($_GET['userid']);
           ?>
         </div>
       </div>
-    <div id="profilePosts-div"class="relative p-auto overflow-auto bg-base-200 min-h-screen mt-8 mb-8">
+    <div id="profilePosts-div"class="relative p-auto overflow-y-auto bg-base-200 min-h-screen mt-8 mb-8">
       <?php
       getPosts($_GET['userid']);
       ?>
