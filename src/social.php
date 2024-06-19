2e263593d5ccb554c79a279ce06382291f6d54a6
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-19 16:01:39 +0100

    Now good

--- src/social.php
+++ src/social.php
@@ -1,13 +1,3 @@
-<?php
-include "include/config.inc.php";
-include "include/functions/checkLogin.inc.php";
-require "include/functions/checkThemeIsFinished.inc.php";
-if (checkThemeIsFinished()) {
-  include "include/functions/saveLastPage.inc.php";
-}
-require "include/functions/Development.inc.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -57,46 +47,46 @@ require "include/functions/Development.inc.php";
   }
   ?>
   <div class="bg-gray-900 fixed w-full md:w-9/12 p-0 m-0 md:right-0 h-full flex flex-col justify-center items-center" id="home-div"></div>
-    <div class="h-32 text-center sm:text-start w-full p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 shadow-mdbackdrop-blur-md">
-      <a href="javascript:history.back()" class="btn">Voltar atrás</a>
-      Home
-    </div>
-    <div class="h-full border-black w-full px-10 overflow-auto">
-      <?php
-      getHome();
-      ?>
-    </div>
-    <dialog id="postReport" class="modal">
-      <div class="modal-box">
-        <form method="dialog">
-          <input type="hidden" id="postId" name="postId">
-          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-gray-900" onclick="cancel()">✕</button>
-          <h3 class="font-bold text-lg">Report</h3>
-          <input type="text" class="hidden" disabled val="" id="reportId">
-          <div class="form-control">
-            <label class="label"></label>
-              <span id="Post-Name" class="label-text">Porque quer reportar: Nome do Post</span>
-            </label>
-            <input type="text" value="" id="post-reason" required class="input input-bordered w-full text-gray-900" placeholder="Insira o motivo de report">
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Tipo</span>
-            </label>
-            <select id="postType" required class="select select-bordered w-full text-gray-900">
-              <option disabled selected>Escolha o tipo</option>
-              <option value="type1">Conteúdo Inapropriado</option>
-              <option value="type2">Não tem a ver com o tema</option>
-              <option value="type3">Plágio</option>
-            </select>
-          </div>
-          <div class="form-control mt-4 flex flex-row"></div>
-            <button class="btn flex-initial btn-warning text-orange-500" onclick="saveReport()">Report</button>
-            <button class="btn btn-outline ml-2 flex-initial btn-error text-red-700" onclick="cancelReport()">Cancelar</button>
-          </div>
-        </form>
+  <div class="h-32 text-center sm:text-start w-full p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 shadow-mdbackdrop-blur-md">
+    <a href="javascript:history.back()" class="btn">Voltar atrás</a>
+    Home
+  </div>
+  <div class="h-full border-black w-full px-10 overflow-auto">
+    <?php
+    getHome();
+    ?>
+  </div>
+  <dialog id="postReport" class="modal bg-base-100">
+  <div class="modal-box">
+    <form method="dialog">
+      <input type="hidden" id="postId" name="postId">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 " onclick="cancel()">✕</button>
+      <h3 class="font-bold text-lg ">Report</h3>
+      <input type="text" class="hidden" disabled val="" id="reportId">
+      <div class="form-control mt-4">
+        <label class="label">
+          <span id="Post-Name" class="label-text ">Porque quer reportar: Nome do Post</span>
+        </label>
+        <input type="text" value="" id="post-reason" class="input input-bordered w-full  bg-base-200" placeholder="Insira o motivo de report">
       </div>
-    </dialog>
+      <div class="form-control mt-4">
+        <label class="label">
+          <span class="label-text ">Tipo</span>
+        </label>
+        <select id="postType" class="select select-bordered w-full  bg-base-200">
+          <option disabled selected>Escolha o tipo</option>
+          <option value="type1">Conteúdo Inapropriado</option>
+          <option value="type2">Não tem a ver com o tema</option>
+          <option value="type3">Plágio</option>
+        </select>
+      </div>
+      <div class="form-control mt-4 flex flex-row gap-2">
+        <button class="btn flex-initial text-orange-500 " onclick="saveReport()">Report</button>
+        <button class="btn btn-outline ml-2 flex-initial btn-error text-red-500" onclick="cancelReport()">Cancelar</button>
+      </div>
+    </form>
+  </div>
+</dialog>
   </div>
   <?php echoBottomNav(); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
