fa17fb5d42bf2c7e0ff20467420f7865832ecb11
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-06-20 21:59:07 +0100

    And now

--- src/js/like.js
+++ src/js/like.js
@@ -1,35 +1,30 @@
 function likeCheck(postid) {
-    console.log('Like button clicked');
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '../src/include/functions/SQLfunctions.inc.php', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
         var response = this.responseText;
         if (this.status == 200) {
-            console.log('Response from server:', this.responseText);
-            console.log('Trimmed response:', this.responseText.trim());
             // Change the text of the like button based on the response
             var likeButton = document.getElementById('like-button-'+postid);
             if (this.responseText.trim() === 'like') {
                 likeButton.textContent = 'Gosto';
             } else if (this.responseText.trim() === 'liked') {
-                likeButton.textContent = 'Gostado';
+                likeButton.textContent = 'Gostou';
             }
         }
         getLikeCounts(postid);
     };
     xhr.send('function=likeCheck&postid=' + encodeURIComponent(postid));
 };
+
 function likeCheckOnLoad(postid) {
-    console.log('Page loaded');
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '../src/include/functions/SQLfunctions.inc.php', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
         var response = this.responseText;
         if (this.status == 200) {
-            console.log('Response from server:', this.responseText);
-            console.log('Trimmed response:', this.responseText.trim());
             // Change the text of the like button based on the response
             var likeButton = document.getElementById('like-button-'+postid);
             if (this.responseText.trim() === 'like') {
@@ -41,18 +36,14 @@ function likeCheckOnLoad(postid) {
     };
     xhr.send('function=likeCheckLoad&postid=' + encodeURIComponent(postid));
 };
+
 function getLikeCounts(postid) {
-    console.log('Getting like counts');
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '../src/include/functions/SQLfunctions.inc.php', true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onload = function() {
-        console.log('entered');
         try {
             if (this.status == 200) {
-                console.log('Response from server:', this.responseText);
-                // Parse the response
-                var response = this.responseText;
                 // Update the likes count on the page
                 document.getElementById('like-count-'+postid).textContent = 'Gostos: ' + response.trim();
             }
@@ -63,13 +54,12 @@ function getLikeCounts(postid) {
     };
     xhr.send('function=likeCount&postid=' + encodeURIComponent(postid));
 };
-window.addEventListener('load', function() {
 
+window.addEventListener('load', function() {
     // Get all post elements
     const posts = document.querySelectorAll('[id^="post-"]');
     // Loop over each post
     posts.forEach(post => {
-        console.log('Post:', post);
         // Get the post id from the element id
         const postId = post.id.split('-')[1];
         // Get the like counts for this post
@@ -79,14 +69,9 @@ window.addEventListener('load', function() {
 });
 
 function openReport(reportId, reportName) {
-    console.log('Type of reportId:', typeof reportId); // Debugging: Check the type
-    console.log('Value of reportId:', reportId); // Debugging: Check the value
-
     document.getElementById('Post-Name').textContent = "Porque quer reportar: '" + reportName + "'?";
-
     // Directly encode reportId without checking its type
     document.getElementById('reportId').value = btoa(reportId.toString()); // Update ID to postId
-
     // Show the modal
     document.getElementById('postReport').showModal(); // Update modal ID to postReport
 }
@@ -105,8 +90,14 @@ function cancelReport() {
 function saveReport() {
     var reportIdElement = document.getElementById('reportId');
     var reportId = atob(reportIdElement.value); // Decode the report ID
-    var reportReason = $('#post-reason').val(); // Get the reason for the report
+    var reportReason = $('#post-reason').val().trim(); // Get the reason for the report and trim whitespace
     var reportType = $('#postType').val(); // Get the selected report type
+
+    // Check if the report reason is empty or if the report type is not selected
+    if (reportReason === "" || reportType === null) {
+        alert('Please fill in all required fields.');
+        return; // Exit the function if any field is empty
+    }
 
     var confirmation = confirm('Are you sure you want to submit this report?');
     if (!confirmation) {
