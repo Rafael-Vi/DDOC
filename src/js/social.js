a4fd73673a2a8dbcefb25e3aa57e910bb38fb606
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-22 00:54:33 +0100

    Logout Works

--- src/js/social.js
+++ src/js/social.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
     $('#logout-button').click(function(e) {
         e.preventDefault(); // Prevent the default anchor behavior
         if (confirm('Are you sure you want to log out?')) {
-            window.location.href = '../src/include/functions/logout.inc.php';
+            window.location.href = '/src/include/functions/logout.inc.php';
         }
     });
 });
