a8b637e5ba097d9dd0e375c50d8d8e6a00c5c813
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-21 23:12:49 +0100

    Roto

--- src/include/config.inc.php
+++ src/include/config.inc.php
@@ -1,4 +1,7 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
     @session_start();
 
     require_once "functions/paths.inc.php";
@@ -14,7 +17,4 @@
     if(basename($_SERVER['PHP_SELF']) !== 'messages.php'){
         unset($_SESSION['sender']);
         unset($_SESSION['convo_id']);
-
     }
-
-
