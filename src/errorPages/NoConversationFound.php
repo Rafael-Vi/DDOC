086795c48be60dabb780bdff94db8e715476905d
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-25 11:13:05 +0100

    testar

--- src/errorPages/NoConversationFound.php
+++ src/errorPages/NoConversationFound.php
@@ -4,11 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/src/css/social.css">    
-    <link rel="stylesheet" href="/src/css/errorPages.css">    
-    <link rel="shortcut icon" href="/src/assets/images/2.png" >
-    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
-    <script src="https://cdn.tailwindcss.com"></script> <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <?php require '/src/errorPages/errorPages-header.php';?>
     <title>Essa Conversa não existe!!!</title>
 </head>
 <body class="bg-gray-800">
