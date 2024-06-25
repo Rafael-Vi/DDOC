99ad4c8fb78f26b5d0ea1b95388e8d3e0fe11cdb
Rafael-Vi <138569272+Rafael-Vi@users.noreply.github.com>
2024-06-25 21:01:43 +0100

    ricardo

--- src/accountLC.php
+++ src/accountLC.php
@@ -69,7 +69,7 @@ if(isset($_SESSION['error'])) {
                     <button type="submit" name="submit" value="registerSubmit" class="w-full px-4 py-2 text-white bg-gray-800 rounded-md hover:bg-orange-700 select-none">Registar</button>
                 </div>
             </form>
-            <a class="flipbutton justify-center flex mt-4 select-none text-warning" id="registerButton" href="#">Login na minah conta</a>
+            <a class="flipbutton justify-center flex mt-4 select-none text-warning" id="registerButton" href="#">Login na minha conta</a>
         </div>
     </div>
 </div>
