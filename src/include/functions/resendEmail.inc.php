b126d7d954fb45b89c6fd0d7e613b26d5865d0a2
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-07-01 14:02:09 +0100

    debug

--- src/include/functions/resendEmail.inc.php
+++ src/include/functions/resendEmail.inc.php
@@ -2,12 +2,15 @@
 require_once "../config.inc.php";
 
 if (isset($_POST['email']) && !empty($_POST['email'])) {
+    echo "Email veriasda.";
     $email = preg_replace('/\s+/', '', htmlspecialchars($_POST['email']));
     if (isset($_POST['action']) && $_POST['action'] == 'email-resend') {
         verifyEmailExistsAndStatus($email);
+        echo "Email verification sent.";
     } elseif (isset($_POST['action']) && $_POST['action'] == 'password-alter' && isset($_POST['newPassword'])) {
         $newPassword = $_POST['newPassword'];
         userPasswordEmail($email, $newPassword);
+        echo "Password change verification email sent.";
     }
 }
 
@@ -23,6 +26,7 @@ function verifyEmailExistsAndStatus($email) {
         $userData = $result->fetch_assoc();
         
         if ($userData['email_verify']) {
+            echo "Email already verified.";
             $_SESSION['error'] = "Este email já foi verificado.";
         } else {
             $username = $userData['user_name'];
@@ -31,12 +35,15 @@ function verifyEmailExistsAndStatus($email) {
             $verificationLink = "http://gentl.store/src/include/functions/verifyEmail.php?username=" . $encryptedUsername . "&email=" . $encryptedEmail;
             
             if (sendVerificationEmail($email, "Verificaçao de Email", "Por favor, verifique seu email para efetuar login.", $verificationLink)) {
+                echo "Verification email sent.";
                 $_SESSION['success'] = "Verifique seu email para concluir o registro.";
             } else {
+                echo "Failed to send verification email.";
                 $_SESSION['error'] = "Falha ao enviar email de verificação.";
             }
         }
     } else {
+        echo "Email not found.";
         $_SESSION['error'] = "Email não encontrado.";
     }
     header("Location: /login");
@@ -63,11 +70,14 @@ function userPasswordEmail($email, $newPassword) {
         $verificationLink = "http://gentl.store/src/include/functions/verifyChangePassword.inc.php?username=" . urlencode($encryptedUsername) . "&email=" . urlencode($encryptedEmail) . "&newPassword=" . urlencode($encryptedNewPassword);
 
         if (sendVerificationEmail($email, "Mudar Password", "Se não tiver pedido a mudança da pssword da sua conta não clicque neste link.", $verificationLink)) {
+            echo "Password change verification email sent.";
             $_SESSION['success'] = "A verification link has been sent to your email.";
         } else {
+            echo "Failed to send password change verification email.";
             $_SESSION['error'] = "Failed to send email with the verification link.";
         }
     } else {
+        echo "Email not found.";
         $_SESSION['error'] = "Email not found.";
     }
     header("Location: /login");
