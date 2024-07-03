52c45fae0a8f61fa1cb344207ebe1645ded76be4
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-07-03 14:49:40 +0100

    HM HM

--- src/js/accountLC.js
+++ src/js/accountLC.js
@@ -1,12 +1,11 @@
-
-// Toggle the flip class on the flipper element when the login button is clicked
+// Add the flip class on the flipper element when the login button is clicked
 var loginButton = document.getElementById("loginButton");
 loginButton.onclick = function() {
-  document.querySelector("#flipper").classList.toggle("flip");
+  document.querySelector("#flipper").classList.add("flip");
 }
 
-// Toggle the flip class on the flipper element when the register button is clicked
+// Remove the flip class from the flipper element when the register button is clicked
 var registerButton = document.getElementById("registerButton");
 registerButton.onclick = function() {
-  document.querySelector("#flipper").classList.toggle("flip");
+  document.querySelector("#flipper").classList.remove("flip");
 }
