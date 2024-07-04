44588d2a2a069a1bfe4967ebf5b2ce5ce96ce745
Rafael Vieira <rafa.pinto.vieira@gmail.com>
2024-07-04 15:33:26 +0100

    Should be  nice

--- src/rankings.php
+++ src/rankings.php
@@ -125,7 +125,7 @@ $GLOBALS['type'] = $type;
 
   ?>
   <div id="Accrankings-div" class=" bg-gray-900 fixed flex flex-col h-full w-full md:w-9/12 p-0 m-0 md:right-0 overflow-auto">
-    <div class=" z-8 h-32 text-center sm:text-start w-full  bg-gray-800 p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 backdrop-blur-md">
+    <div class="z-20 h-32 text-center sm:text-start w-full  bg-gray-800 p-10 font-bold text-4xl text-white sticky top-0 flex items-center justify-left gap-8 backdrop-blur-md">
       <a href="javascript:history.back()" class="btn">Voltar atrás</a>
       Ranking de Contas
     </div>
